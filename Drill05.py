b2fa156d1f22015f6dd911b3740d3a46872d4e81
JeongJinbong(NoteBook) <jbong0104@naver.com>
2023-09-28 15:11:00 +0900

    hand moving

--- Drill05.py
+++ Drill05.py
@@ -1,8 +1,6 @@
 import random
 from pico2d import *
 
-from move_character_with_mouse import handle_events
-
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
@@ -12,9 +10,20 @@ hand_arrow = load_image('hand_arrow.png')
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-hx, hy = random.randint(0,1200), random.randint(0,1150)
-cx, cy = random.randint(0,1200), random.randint(0,1150)
+hx, hy = random.randint(0,1200), random.randint(0,1100)
+cx, cy = random.randint(0,1200), random.randint(0,1100)
 frame = 0
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
 
 def movetohand():
     global cx, cy
@@ -35,10 +44,12 @@ def movetohand():
             character.clip_draw(frame * 100, 0 * 1, 100, 100, movex, movey)
         update_canvas()
         frame = (frame + 1) % 8
-        handle_events()
         delay(0.016)
+        if i == 100-1:
+            hx, hy = random.randint(0, 1200), random.randint(0, 1100)
+
 
 while running:
     movetohand()
-    update_canvas()
+    handle_events()
 close_canvas()
